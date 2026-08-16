c2e5a57a7290c050e6ddfe09123b261b6cdd9ff4
mvsrcdst <43897151+mvsrcdst@users.noreply.github.com>
2026-08-16 19:03:24 +0300

    fix: ggml detection

--- Telegram/SourceFiles/ayu/features/stt/ggml_backend_detector.cpp
+++ Telegram/SourceFiles/ayu/features/stt/ggml_backend_detector.cpp
@@ -16,13 +16,17 @@ namespace {
 #if defined(Q_OS_WIN)
 constexpr auto kCudaDriverLib = "nvcuda";
 constexpr auto kVulkanLoaderLib = "vulkan-1";
+constexpr auto kLibraryVersion = -1;
 #else
-constexpr auto kCudaDriverLib = "cuda.1";
-constexpr auto kVulkanLoaderLib = "vulkan.1";
+constexpr auto kCudaDriverLib = "cuda";
+constexpr auto kVulkanLoaderLib = "vulkan";
+constexpr auto kLibraryVersion = 1;
 #endif
 
-bool ProbeLibraryPresent(const char *name) {
-	QLibrary lib(QString::fromUtf8(name));
+bool ProbeLibraryPresent(const char *name, const int version) {
+	QLibrary lib = (version == -1)
+		? QLibrary(QString::fromUtf8(name))
+		: QLibrary(QString::fromUtf8(name), version);
 	const auto loaded = lib.load();
 	if (loaded) {
 		lib.unload();
@@ -35,9 +39,9 @@ bool ProbeLibraryPresent(const char *name) {
 bool IsGpuDriverPresent(const GgmlBackendKind kind) {
 	switch (kind) {
 	case GgmlBackendKind::CUDA:
-		return ProbeLibraryPresent(kCudaDriverLib);
+		return ProbeLibraryPresent(kCudaDriverLib, kLibraryVersion);
 	case GgmlBackendKind::Vulkan:
-		return ProbeLibraryPresent(kVulkanLoaderLib);
+		return ProbeLibraryPresent(kVulkanLoaderLib, kLibraryVersion);
 	case GgmlBackendKind::Metal:
 #if defined(Q_OS_MAC)
 		return true;
